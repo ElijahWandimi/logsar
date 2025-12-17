39557e0f64f92d5c984c7b407873a4f519d8d917
ElijahWandimi <wandimiem7@gmail.com>
2025-12-17 20:57:14 +0000

    print to stdout works

---
+++ include/formatter.h
@@ -0,0 +1,13 @@
+//
+// Created by elijah on 11/23/25.
+//
+
+#ifndef LOGSAR_FORMATTER_H
+#define LOGSAR_FORMATTER_H
+
+namespace logsar {
+    class Formatter {}
+}
+
+
+#endif //LOGSAR_FORMATTER_H
---
+++ src/utils/formatter.cpp
@@ -0,0 +1,7 @@
+//
+// Created by elijah on 11/23/25.
+//
+
+#include "formatter.h"
+
+namespace logsar {}
